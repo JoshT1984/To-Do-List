36d8fc5b395e7a16ec85afeb60d6faa21a96f759
JoshT1984 <warlord84502@yahoo.com>
2023-01-08 21:29:49 -0600

    Added process.env.PORT to index.js

--- public/js/index.js
+++ public/js/index.js
@@ -66,6 +66,6 @@ app.delete("/:id", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT|| 3000, () => {
   console.log("Listening on port 3000");
 });
